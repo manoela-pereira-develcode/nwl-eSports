28b4e5e5679bad2e1cfef3709e9a9cc1886c3375
Manoela Pereira <manoela.pereira@develcode.com.br>
2022-09-16 16:47:50 -0300

    aula 2

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
 
   ],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"]
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
